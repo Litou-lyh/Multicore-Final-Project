f81bec318ab961ef0c3fa447208636bc645d9b8a
Sid Wang <zw2686@cuda5.cims.nyu.edu>
2023-11-28 18:11:55 -0500

    tests

--- test.bash
+++ test.bash
@@ -7,7 +7,7 @@ declare -A directories=(
 )
 
 # Define the sizes
-keypoint_sizes=(1000 2000 3000 4000 5000)
+keypoint_sizes=(1000 2000 3000 4000 5000 6000 7000 8000 9000 10000)
 
 # Output directory
 output_dir="/scratch/Multicore-Final-Project/tests/"
